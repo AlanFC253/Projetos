4b6b43e4b801a17c13fab2ede71707db517f0a62
AlanFC253 <ala253523@live.com>
2023-03-08 21:03:35 -0300

    Jogo da velha
    Jogo da velha feito totalmente a mão, porem copiado, credito nos comentario da class Main

---
+++ Projetos_Java/Jogo_da_Velha/Main.java
@@ -0,0 +1,8 @@
+class Main{
+
+
+    // Jogo feito todo a mão, porem copiado pelo site: https://www.javaprogressivo.net/2012/10/Como-fazer-um-Jogo-da-Velha-em-Java.html
+    public static void main(String[] args) {
+      Jogo jogo = new Jogo();
+    }
+}
